8c3d540036d511351d66b7cc19a0b70eb9305c7b
nabeel-wq <nabeelahmedansari.7777@gmail.com>
2025-05-08 03:18:10 +0530

    Add files via upload

---
+++ neural_net_modular.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+# Activation functions and their derivatives
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+def sigmoid_prime(z):
+    s = sigmoid(z)
+    return s * (1 - s)
+
+def relu(z):
+    return np.maximum(0, z)
+
+def relu_prime(z):
+    return (z > 0).astype(float)
+
+# Cost functions
+def mse_cost(output_activations, y):
+    return 0.5 * np.linalg.norm(output_activations - y) ** 2
+
+def mse_delta(output_activations, y, z, activation_prime):
+    return (output_activations - y) * activation_prime(z)
+
+class ModularNetwork:
+    def __init__(self, sizes, activation=sigmoid, activation_prime=sigmoid_prime,
+                 cost=mse_cost, delta=mse_delta):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+        self.activation = activation
+        self.activation_prime = activation_prime
+        self.cost = cost
+        self.delta = delta
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = self.activation(np.dot(w, a) + b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta):
+        n = len(training_data)
+        for j in range(epochs):
+            np.random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k + mini_batch_size]
+                for k in range(0, n, mini_batch_size)
+            ]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            print(f"Epoch {j} complete")
+
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w - (eta / len(mini_batch)) * nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(mini_batch)) * nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = self.activation(z)
+            activations.append(activation)
+
+        delta = self.delta(activations[-1], y, zs[-1], self.activation_prime)
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = self.activation_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_b, nabla_w)
